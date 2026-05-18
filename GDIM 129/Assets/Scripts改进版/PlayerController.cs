f13b84f9eec646f0ed60779d783a5ee8d336157e
Martin-aaa <ruichem2@uci.edu>
2026-05-17 21:42:14 -0700

    5.17

--- GDIM 129/Assets/Scripts改进版/PlayerController.cs
+++ GDIM 129/Assets/Scripts改进版/PlayerController.cs
@@ -6,14 +6,29 @@ public class PlayerController : Singleton<PlayerController> {
     private Vector2 _moveDirection;
     [SerializeField] private float _moveSpeed = 3f;
     public Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
+        MoveControll();
+    }
+
+    void MoveControll()
+    {
         _moveDirection.x = _moveSmooth ? Input.GetAxis("Horizontal") : Input.GetAxisRaw("Horizontal");
 
-        rb.linearVelocityX = _moveDirection.x * _moveSpeed;
+            rb.linearVelocityX = _moveDirection.x * _moveSpeed;
+        if (_moveDirection.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (_moveDirection.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
     }
 }

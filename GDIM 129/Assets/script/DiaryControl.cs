74beba001e474b422b24c131c1c856e53c4d6bdc
Martin-aaa <ruichem2@uci.edu>
2026-05-30 21:22:24 -0700

    5.30
    日记翻页系统完善

--- GDIM 129/Assets/script/DiaryControl.cs
+++ GDIM 129/Assets/script/DiaryControl.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class DiaryControl : Singleton<DiaryControl>
 {
@@ -8,6 +9,7 @@ public class DiaryControl : Singleton<DiaryControl>
     public List<GameObject> _SpawnedDiaryPages = new List<GameObject>();
     public bool _DiaryOpen = false;
     public GameObject ParentObject;
+    private int _currentPageIndex = 0;
 
     private void Awake()
     {
@@ -32,7 +34,24 @@ public class DiaryControl : Singleton<DiaryControl>
         int index = DaytimeControl.Instance._Daytime - 1;
         GameObject newPage = Instantiate(_diaryPagePrefabs[index], ParentObject.transform);
         _SpawnedDiaryPages.Add(newPage);
+        newPage.SetActive(false);
 
+    }
+
+    public void NextPage()
+    {
+        if (_SpawnedDiaryPages.Count == 0|| _SpawnedDiaryPages == null)return;
+        _SpawnedDiaryPages[_currentPageIndex].SetActive(false);
+        _currentPageIndex = (_currentPageIndex + 1) % _SpawnedDiaryPages.Count;
+        _SpawnedDiaryPages[_currentPageIndex].SetActive(true);
+    }
+
+    public void PreviousPage()
+    {
+        if (_SpawnedDiaryPages.Count == 0 || _SpawnedDiaryPages == null) return;
+        _SpawnedDiaryPages[_currentPageIndex].SetActive(false);
+        _currentPageIndex = (_currentPageIndex - 1 + _SpawnedDiaryPages.Count) % _SpawnedDiaryPages.Count;
+        _SpawnedDiaryPages[_currentPageIndex].SetActive(true);
     }
 }
 
